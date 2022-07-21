1aa03c484643c2448cfc722655103ffecb82892b
sudharsan9500 <sridharankavitha2@gmail.com>
2022-07-21 15:38:55 +0530

    first code

---
+++ src/testing/Encapulationtesting.java
@@ -0,0 +1,28 @@
+package testing;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Encapulationtesting {
+
+	public static void main(String[] args) {
+	     
+		Map<Employee,Sudh> m = new LinkedHashMap<Employee,Sudh>();
+	    Set<Employee> keySet = m.keySet();
+	    System.out.println(keySet);
+	    for (Employee employee : keySet) {
+			System.out.println(employee);
+		}
+		
+	
+			
+		}
+		
+		
+		}
+		
+
+	
+
+
---
+++ src/testing/Sudh.java
@@ -0,0 +1,32 @@
+package testing;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class Sudh{
+	private String s = "sudha";
+
+	public String getS() {
+		return s;
+	}
+
+	public void setS(String s) {
+		this.s = s;
+	}
+
+	public Sudh(String s) {
+		super();
+		this.s = s;
+	}
+	
+		
+	
+
+	
+    
+}
+	
+	
+	
+
+
